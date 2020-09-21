93db7fb4173d622c0208b49b5e8925269a6dc695
tolaoniyangi <tolaoniyangi.com>
2020-09-21 15:41:26 -0400

    added basic leaflet map

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-from flask import Flask, request, render_template, session, redirect
-import numpy as np
-import pandas as pd
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
-                   'B': [5, 6, 7, 8, 9],
-                   'C': ['a', 'b', 'c--', 'd', 'e']})
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-
-@app.route('/', methods=("POST", "GET"))
-def html_table():
-    return df.to_html(header="true", table_id="table")
-
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
+
+# from flask import Flask, request, render_template, session, redirect
+# import numpy as np
+# import pandas as pd
+
+
+# app = Flask(__name__)
+
+# df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+#                    'B': [5, 6, 7, 8, 9],
+#                    'C': ['a', 'b', 'c--', 'd', 'e']})
+
+
+# @app.route('/', methods=("POST", "GET"))
+# def html_table():
+#     return df.to_html(header="true", table_id="table")
+
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
